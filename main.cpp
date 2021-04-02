088513b86fc7454714d4bf1efd63e29db8ec4479
Chanawee124 <>
2021-04-02 13:37:01 +0000

    finished

--- main.cpp
+++ main.cpp
@@ -6,13 +6,11 @@ using namespace std;
 
 int main() {
   Time t1,t2,t3;
-       cout<< "What time was it?";
-    t1.getTime();
+     cout<< "What time was it?";
+     t1.getTime();
      cout<< "What time is it now?";
      t2.getTime();
      t3=t2-t1;
-     t1.display();
-     t2.display();
-    t3.display();
+     t3.display();
    return 0;
 }
